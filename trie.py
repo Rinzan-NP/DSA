ad3a7c848febca9e4ca858d2cbaec8aba09f9789
Rinzan-NP <nprinzan@gmail.com>
2023-11-15 13:15:56 +0530

    trie added removing searching

---
+++ trie.py
@@ -0,0 +1,61 @@
+class TrieNode:
+    def  __init__(self) -> None:
+        self.children = {}
+        self.end_word = False
+
+class Trie:
+    def __init__(self) -> None:
+        self.root = TrieNode()
+
+    def add(self, word : str):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.end_word = True
+
+    def search(self, word : str):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.end_word  
+
+    def remove(self, word):
+        self.recursive_remove(self.root, word, 0)
+
+    def recursive_remove(self, node : TrieNode, word, index):
+        if index == len(word):
+            if not node.end_word :
+                return False
+            node.end_word = False
+            return len(node.children) == 0
+        
+        char = word[index]
+        if char not in node.children:
+            return False
+        
+        should_delete = self.recursive_remove(node.children[char], word, index + 1)
+
+        if should_delete :
+            del node.children[char]
+            return len(node.children) == 0 
+        
+        return False
+    
+trie = Trie()
+print("Searching apple in trie : " + str({trie.search("apple")}))
+trie.add("apple")
+print("apple added!")
+print("Searching apple in trie : " + str({trie.search("apple")}))
+trie.remove("apple")
+print("apple deleted!")
+print("Searching apple in trie : " + str({trie.search("apple")}))
+
+        
+
+
+        
+
