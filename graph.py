a69b15e2eb25097a9707d39a1bb89ea93c0d3098
Rinzan-NP <nprinzan@gmail.com>
2023-11-16 12:16:36 +0530

    depth first search

--- graph.py
+++ graph.py
@@ -37,17 +37,54 @@ class Graph:
                 paths.append(new_path)
 
         return paths
+    
+    def depth_first_search(self, start, visited=None):
+        if visited is None:
+            visited = []
+
+        if start not in self.graph:
+            return []
+        
+        
+        if start not in visited:
+            visited.append(start)
+            for neighbor in self.graph[start]:
+                self.depth_first_search(neighbor, visited)
+
+        for vertex in self.graph:
+            if vertex not in visited:
+                self.depth_first_search(vertex, visited)
+
+        return visited
 
 
-# graph = Graph()
-# graph.add(1, 2, uni = True)
-# graph.add(1, 3, uni = True)
-# graph.add(2, 3, uni = True)
-# graph.add(2, 4, uni = True)
-# graph.add(3, 4, uni = True)
-# start = 1
-# end = 4
-# print(f"Path from {start} to  {end} are : {graph.find_path(start, end)}")
-# print(f"Before deleting : {graph.graph}")
-# graph.remove_vertex(4)
-# print(f"After deleting 4 : {graph.graph}")
+
+graph = Graph()
+
+graph.add(1, 3, uni = True)
+graph.add(2, 3, uni = True)
+graph.add(2, 4, uni = True)
+graph.add(3, 4, uni = True)
+graph.add(5,6)
+start = 1
+end = 4
+print(graph.graph)
+print(graph.depth_first_search(1))
+
+def depth_first_search(self, start, visited=None):
+    if visited is None:
+        visited = []
+
+    if start not in self.graph:
+        return []
+
+    if start not in visited:
+        visited.append(start)
+        for neighbor in self.graph[start]:
+            self.depth_first_search(neighbor, visited)
+
+    for vertex in self.graph:
+        if vertex not in visited:
+            self.depth_first_search(vertex, visited)
+
+    return visited
