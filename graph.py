9bdfe5494039b476a9304395b84690839d97540c
Rinzan-NP <nprinzan@gmail.com>
2023-11-16 13:38:28 +0530

    breadth first search

--- graph.py
+++ graph.py
@@ -1,3 +1,4 @@
+from collections import deque
 class Graph:
     def __init__(self) -> None:
         self.graph = {}
@@ -56,7 +57,24 @@ class Graph:
                 self.depth_first_search(vertex, visited)
 
         return visited
+    
+    def breadth_first_search(self, start, visited = None):
+        if visited is None:
+            visited = []
+        queue = deque([start])
 
+        while queue:
+            vertex = queue.popleft()
+            if vertex not in visited:
+                visited.append(vertex)
+                queue.extend(self.graph[vertex])
+
+        for vertex in self.graph:
+            if vertex not in  visited:
+                self.breadth_first_search(vertex, visited)
+
+
+        return visited
 
 
 graph = Graph()
@@ -66,25 +84,10 @@ graph.add(2, 3, uni = True)
 graph.add(2, 4, uni = True)
 graph.add(3, 4, uni = True)
 graph.add(5,6)
+graph.add(2,6)
 start = 1
 end = 4
 print(graph.graph)
-print(graph.depth_first_search(1))
+print(graph.breadth_first_search(1))
 
-def depth_first_search(self, start, visited=None):
-    if visited is None:
-        visited = []
 
-    if start not in self.graph:
-        return []
-
-    if start not in visited:
-        visited.append(start)
-        for neighbor in self.graph[start]:
-            self.depth_first_search(neighbor, visited)
-
-    for vertex in self.graph:
-        if vertex not in visited:
-            self.depth_first_search(vertex, visited)
-
-    return visited
